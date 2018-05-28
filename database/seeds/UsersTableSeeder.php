477e1937a80058b905873df4048fe05e278340c8
Miguekos <miguekos1233@gmail.com>
2018-05-27 22:11:24 -0500

    Deploying to Heroku1

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,9 +13,9 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         // Let's clear the users table first
-        User::truncate();
-
-        $faker = \Faker\Factory::create();
+//        User::truncate();
+//
+//        $faker = \Faker\Factory::create();
 
         // Let's make sure everyone has the same password and
         // let's hash it before the loop, or else our seeder
