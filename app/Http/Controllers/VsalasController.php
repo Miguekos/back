a26047f2e7798463054fcae914144639cbc69c48
Unknown <miguekos1233@gmail.com>
2018-05-30 00:51:15 -0500

    Fail al editar.
    Success

--- app/Http/Controllers/VsalasController.php
+++ app/Http/Controllers/VsalasController.php
@@ -49,7 +49,7 @@ class VsalasController extends Controller
     public function update(Request $request, $id){
 
         $this->validate($request, [
-            'nombre' => 'required|min:3|unique:salas',
+            'nombre' => 'required',
             'cant_max' => 'required|integer'
         ]);
 
